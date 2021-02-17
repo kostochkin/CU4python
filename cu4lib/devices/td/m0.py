d19de21360a0940fd757386767195f6e60dde04f
Konstantin Gorshkov <k.n.gorshkov@gmail.com>
2021-02-17 18:06:23 +0300

    split constants and tdm0

--- cu4lib/devices/td/m0.py
+++ cu4lib/devices/td/m0.py
@@ -1,5 +1,6 @@
 from cu4lib.devices.cu4device import CU4Device
 from cu4lib.simplelog import StdioLogger
+from cu4lib.devices.constants import CU4DeviceGeneralConstant, CU4DeviceGeneralConstantCopy
 
 
 class TemperatureTableException(Exception):
@@ -27,7 +28,7 @@ class CU4DeviceTDM0(CU4Device):
 
     @property
     def data(self):
-        return CU4DeviceTDM0(super().data)
+        return CU4DeviceTDM0Data(super().data)
 
     @property
     def thermometer(self):
@@ -83,7 +84,6 @@ class CU4DeviceTDM0(CU4Device):
             return self._set_json("TEMT", tbl_copy.dict())
         else:
             raise TemperatureTableException("The temperature table must contain exactly 100 points")
-
 
 
 class CU4DeviceTDMConstantsCopy:
@@ -142,55 +142,8 @@ class CU4DeviceTDMConstantsCopy:
         return str(self._d)
 
 
-class CU4DeviceTDMConstantCopy:
-    """ CU4DeviceTDMConstantsCopy is intended to store slope and intercept values of a constant.
-        Warning! Damage is possible. Constants are used to configure the module.
-        Do not modify it unless you know what are you doing.
-        
-        Using example
-        -------------
-        Assuming d is a CU4DeviceTDM instance:
-
-        cc = d.constants.copy()
-        cc.current_adc.slope = 1
-        cc.current_adc.intercept = 1
-        d.constants = cc
-    """
-
-    def __init__(self, lst, name):
-        """ Constructor
-
-            Parameters
-            ----------
-            :lst list: an list got from the corresponding server response
-            :name string: constant name in the server response
-        """
-        self._lst = lst
-        self._name = name
-
-    @property
-    def slope(self):
-        """ :slope float: """
-        return self._lst[0]
-    
-    @slope.setter
-    def slope(self, x):
-        self._lst[0] = x
-    
-    @property
-    def intercept(self):
-        """ :intercept float: """
-        return self._lst[1]
-
-    @intercept.setter
-    def intercept(self, x):
-        self._lst[1] = x
-    
-    def __repr__(self):
-        return "<{}(slope={},intercept={})>".format(self._name, self.slope, self.intercept)
-
-    def __iter__(self):
-        return iter(self._lst)
+class CU4DeviceTDMConstantCopy(CU4DeviceGeneralConstantCopy):
+    pass
 
 
 class CU4DeviceTDMConstants:
@@ -280,68 +233,14 @@ class CU4DeviceTDMConstants:
         return "<{}(...lazy...)>".format(self.__class__.__name__)
 
 
-class CU4DeviceTDMConstant:
-    """ CU4DeviceTDMConstant is intended to get and set the constant instantly
-        Warning! Damage is possible. Constants are used to configure the module.
-        Do not modify it unless you know what are you doing.
-    """
-
-    def __init__(self, cu4server, name):
-        self._cu4server = cu4server
-        self._name = name
-        self._commands = {"Current_ADC": "CADC",
-                          "Current_DAC": "CDAC",
-                          "Voltage_ADC": "VADC",
-                          "Press_VoltageP_ADC": "VPPC",
-                          "Press_VoltageN_ADC": "VPNC"}
-    
-    def copy(self):
-        """ Returns
-            -------
-            CU4DeviceTDMConstantCopy
-        """
-        return CU4DeviceTDMConstantCopy(self._getter(), self._name)
-        
-    def values_from_copy(self, c):
-        """ Atomically sets slope and intercept from CU4DeviceTDMConstantCopy
-
-            Parameters
-            ----------
-            :c CU4DeviceTDMConstantCopy :
-        """
-        self._values_from_list(list(c))
-
-    @property
-    def slope(self):
-        """ float """
-        return self._getter()[0]
-    
-    @slope.setter
-    def slope(self, v):
-        return self._setter(0, v)
-
-    @property
-    def intercept(self):
-        """ float """
-        return self._getter()[1]
-    
-    @intercept.setter
-    def intercept(self, v):
-        return self._setter(1, v)
-
-    def _getter(self):
-        return self._cu4server._get_json(self._commands[self._name] + "?")
-
-    def _setter(self, i, v):
-        cur = self._getter()
-        cur[i] = v
-        return self._values_from_list(cur)
-
-    def _values_from_list(self, lst):
-        return self._cu4server._set_json(self._commands[self._name], lst)
-
-    def __repr__(self):
-        return "<{}(...lazy...)>".format(self._name)
+class CU4DeviceTDMConstant(CU4DeviceGeneralConstant):
+    def _commands(self, cmd):
+        commands = {"Current_ADC": "CADC",
+                    "Current_DAC": "CDAC",
+                    "Voltage_ADC": "VADC",
+                    "Press_VoltageP_ADC": "VPPC",
+                    "Press_VoltageN_ADC": "VPNC"}
+        return commands[cmd]
     
 
 class PressureMeter:
